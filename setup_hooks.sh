7d7ec9d1f7a44e30d622f354ca8ed215d1d314e4
znibb <herp@derp.com>
2016-09-29 22:46:35 +0200

    Removed hooks dir and added setup_hooks.sh
    Replaced hooks dir with setup script that takes user input and creates an appropriate hook

---
+++ setup_hooks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VALID_FLAGS="(-rpi)"
+
+# Read user input and build invocation
+echo "Setting up hook to auto-run copytohome.sh on git merge"
+echo "Enter relevant flags "$VALID_FLAGS" or none for base aliases only"
+read flags
+INVOCATION="./copytohome.sh "$flags
+echo "Writing hook invoking: "$INVOCATION
+
+# Create hook
+echo "#!/bin/bash" > .git/hooks/post-merge
+echo >> .git/hooks/post-merge
+echo $INVOCATION >> .git/hooks/post-merge
